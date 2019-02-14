2818e64366d1d968bc186de3bcf82c620ee95a41
memizzos <you@example.com>
2019-02-14 19:49:27 +0100

    xxxx

--- witam_pana.py
+++ witam_pana.py
@@ -2,8 +2,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Witam Pana --name')
 parser.add_argument('--name', type=str, help='Twoje imie prosze Pana')
-
+parser.add_argument('--powitanie', type=str, help='Twoje imie prosze Pana')
 
 args = parser.parse_args()
 
-print('Witam serdecznie pana ' + args.name)
+print('Witaj ' + args.name)
